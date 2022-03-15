52bf88db77949ffa3da59df6175634d83955c2f0
Bartosz Horoba <bhoroba@iteratoshq.com>
2022-03-15 12:37:14 +0100

    Basic project setup - enabling TS and configure files

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    flex: 1,
+  },
+});
+
+export default App;
